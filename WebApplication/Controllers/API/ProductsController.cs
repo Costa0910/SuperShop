491de65f1d4c34f4862d18f1f05141d5c238fb10
ARMANDO DA COSTA <dacostaarmando496@gmail.com>
2024-07-25 15:36:58 +0100

    Add migration and api controller

---
+++ WebApplication/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Data;
+
+namespace WebApplication.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+    }
+}
