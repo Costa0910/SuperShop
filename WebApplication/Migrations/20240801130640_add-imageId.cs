39b0bc9431808d88dce91e0985d84eeb6fa42e9d
ARMANDO DA COSTA <dacostaarmando496@gmail.com>
2024-08-01 14:13:59 +0100

    update database

---
+++ WebApplication/Migrations/20240801130640_add-imageId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class addimageId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "LastParchase",
+                table: "Products",
+                newName: "LastPurchase");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "LastPurchase",
+                table: "Products",
+                newName: "LastParchase");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
