4b0b6253877683aa9e0fc85f212ff451033daeb2
ARMANDO DA COSTA <dacostaarmando496@gmail.com>
2024-08-13 11:48:04 +0100

    add constrain in country´s name

--- WebApplication/Migrations/20240813102920_change-price-colunm-datatype.Designer.cs
+++ WebApplication/Migrations/20240813102920_change-price-colunm-datatype.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication.Data;
 
 namespace WebApplication.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240813102920_change-price-colunm-datatype")]
+    partial class changepricecolunmdatatype
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
